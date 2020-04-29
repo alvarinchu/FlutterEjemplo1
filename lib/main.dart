a1715c4fcd6848d38f51a5744c2ecf01d51e6000
Alvaro Pérez Rodrigo <alvarinchu@gmail.com>
2020-04-29 18:03:07 +0200

    ipad commity 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Welcome to Flutter pruebaa',
+        title: 'Welcome to Flutter pruebas',
         theme: ThemeData(
 			primaryColor: Colors.white,
 		),
